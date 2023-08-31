af48bcf9eb40225858b9a6b4463e9220755e775f
Thorsten Hans <thorsten.hans@gmail.com>
2023-08-31 09:15:25 +0200

    feat: return richer release info and support for canary
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,84 @@
 #![warn(rust_2018_idioms)]
 
-use anyhow::{Result, bail};
+use anyhow::{bail, Result};
 
+use serde::Serialize;
 use spin_sdk::{
-    http::{Request, Response, Router, Params},
-    http_component, outbound_http, config,
+    config,
+    http::{Params, Request, Response, Router},
+    http_component, outbound_http,
 };
 
-fn get_authorization_header_value() -> Result<String> {
-    let token = config::get("github_token")?;
-    Ok(format!("token {}", token))
+/// A simple Spin HTTP component.
+#[http_component]
+fn handle_check_spin_cli_version(req: Request) -> Result<Response> {
+    let mut router = Router::default();
+    router.get("/version/:channel", get_version_by_channel);
+    router.get("/version", get_latest_stable_version);
+    router.handle(req)
 }
 
-fn handle_get_latest_spin_cli_version(_req: Request, _params : Params) -> Result<Response> {
+#[derive(Debug, Serialize)]
+pub struct ReleaseInfo {
+    pub version: String,
+    pub commit_hash: String,
+    pub published_at: String,
+}
+
+impl TryFrom<&Option<bytes::Bytes>> for ReleaseInfo {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &Option<bytes::Bytes>) -> std::result::Result<Self, Self::Error> {
+        match value {
+            Some(body) => {
+                let json = serde_json::from_slice::<serde_json::Value>(body)?;
+                let mut version = json["tag_name"].as_str().unwrap_or("unknown");
+                let commit_hash = json["target_commitish"].as_str().unwrap_or("unknown");
+                let published_at = json["published_at"].as_str().unwrap_or("unknown");
+                if version.starts_with('v') {
+                    version = version[1..].trim();
+                }
+                Ok(ReleaseInfo {
+                    version: version.to_string(),
+                    commit_hash: commit_hash.to_string(),
+                    published_at: published_at.to_string(),
+                })
+            }
+            None => bail!("No body in response"),
+        }
+    }
+}
+
+fn get_latest_stable_version(_req: Request, _params: Params) -> Result<Response> {
+    let ri = get_release_info(None)?;
+
+    Ok(http::Response::builder()
+        .status(http::StatusCode::OK)
+        .header(http::header::CONTENT_TYPE, "text/plain")
+        .body(Some(ri.version.into()))?)
+}
+
+fn get_version_by_channel(_req: Request, params: Params) -> Result<Response> {
+    let c = params.get("channel");
+    let ri = get_release_info(c)?;
+    let body = serde_json::to_string(&ri)?;
+
+    Ok(http::Response::builder()
+        .status(http::StatusCode::OK)
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .body(Some(body.into()))?)
+}
+
+fn get_release_info(channel: Option<&str>) -> Result<ReleaseInfo> {
+    let url = match channel {
+        Some("canary") => "https://api.github.com/repos/fermyon/spin/releases/tags/canary",
+        _ => "https://api.github.com/repos/fermyon/spin/releases/latest",
+    };
     let auth_header_value = get_authorization_header_value()?;
+
     let req = http::Request::builder()
         .method("GET")
-        .uri("https://api.github.com/repos/fermyon/spin/releases/latest")
+        .uri(url)
         .header(http::header::ACCEPT, "application/vnd.github+json")
         .header("X-GitHub-Api-Version", "2022-11-28")
         .header(http::header::USER_AGENT, "spin-cli-version-checker")
@@ -26,43 +88,20 @@ fn handle_get_latest_spin_cli_version(_req: Request, _params : Params) -> Result
     let Ok(res) = outbound_http::send_request(req) else {
         let err = "Could not determine latest Spin CLI release. (Could not send request)";
         println!("{}", err);
-        return Ok(http::Response::builder()
-            .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-            .body(Some(err.into()))?);
+        bail!("{}", err)
     };
     if res.status() != http::StatusCode::OK {
-        let err = format!("Could not determine latest Spin CLI release. ({:?})", res.status());
+        let err = format!(
+            "Could not determine latest Spin CLI release. ({:?})",
+            res.status()
+        );
         println!("{}", err);
-        return Ok(http::Response::builder()
-            .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-            .body(Some(err.into()))?);
+        bail!("{}", err)
     }
-    let version = extract_version(res.body())?;
-    Ok(http::Response::builder()
-        .status(http::StatusCode::OK)
-        .header(http::header::CONTENT_TYPE, "text/plain")
-        .body(Some(version.into()))?)
-    
+    ReleaseInfo::try_from(res.body())
 }
 
-fn extract_version(body: &Option<bytes::Bytes>) -> Result<String> {
-    match body {
-        Some(body) => {
-            let json = serde_json::from_slice::<serde_json::Value>(body)?;
-            let mut version = json["tag_name"].as_str().unwrap_or("unknown");
-            if version.starts_with('v') {
-                version = version[1..].trim();
-            }
-            Ok(version.to_string())
-        },
-        None => bail!("No body in response")
-    }
-}
-/// A simple Spin HTTP component.
-#[http_component]
-fn handle_check_spin_cli_version(req: Request) -> Result<Response> {
-    let mut router = Router::default();
-    router.get("/version", handle_get_latest_spin_cli_version);
-    
-    router.handle(req)
+fn get_authorization_header_value() -> Result<String> {
+    let token = config::get("github_token")?;
+    Ok(format!("token {}", token))
 }
